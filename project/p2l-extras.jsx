7876cd6c3239369b03b04b660d54cb02f82ea510
gdl-joe <git@b-prisma.de>
2026-05-03 14:57:08 +0200

    feat: app shell, PWA service worker, main App component

---
+++ project/p2l-extras.jsx
@@ -0,0 +1,2 @@
+// p2l-extras — map and extra components will be added in Task 14
+// Placeholder: ScreenMap
---
+++ project/p2l-screens.jsx
@@ -0,0 +1,2 @@
+// p2l-screens — screens will be added in Tasks 10-13 and 15
+// Placeholder: ScreenLogin, ScreenList, ScreenDetail, ScreenForm, ScreenStats, ScreenProfile
